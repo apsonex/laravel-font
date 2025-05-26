b2c1fea5b0f92e894c70bb3cd5c77ef6dfdb08c9
Gurinder Chauhan <gurinderchauhan@gmail.com>
2025-05-26 13:30:03 -0400

    update

--- src/LaravelFontServiceProvider.php
+++ src/LaravelFontServiceProvider.php
@@ -9,12 +9,12 @@ class LaravelFontServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        $package->name('laravel-font');
+        $package->name("laravel-font");
     }
 
     public function registeringPackage(): void
     {
-        $this->app->singleton('laravelfont', function () {
+        $this->app->singleton(LaravelFont::class, function () {
             return new LaravelFont(new \Apsonex\Font\Font());
         });
     }
